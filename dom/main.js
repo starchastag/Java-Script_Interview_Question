31eb3233cdb76b7aeb9dada3701332eb7dcc9bd4
shubham kumar <90451740+starchastag@users.noreply.github.com>
2023-10-04 12:56:28 +0530

    notes with  practise

---
+++ dom/main.js
@@ -0,0 +1,19 @@
+// getting element by using queryselcetor
+
+// query selector return fist element that match css selector
+
+// To get all matched element then we use queryselector all
+
+// let resultElement = document.querySelectorAll('p')
+
+
+// console.log(resultElement);
+
+// Acess element with class
+let resultElement = document.querySelector('.para');
+
+console.log(resultElement); 
+
+// Acess element by id
+let resultElements = document.querySelector('#shead');
+console.dir(resultElements);
